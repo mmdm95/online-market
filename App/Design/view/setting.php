7d7725a5db502f0b06fc2d00c12e947c515bad79
MMDM <mmdehghan95@gmail.com>
2020-12-04 23:51:27 +0330

    add custom image placeholder

--- App/Design/view/setting.php
+++ App/Design/view/setting.php
@@ -7,9 +7,12 @@
 
                 <div class="card-body">
                     <ul class="nav nav-tabs nav-tabs-bottom nav-justified">
-                        <li class="nav-item"><a href="#bottom-justified-tab1" class="nav-link active" data-toggle="tab">تنظیمات عمومی</a></li>
-                        <li class="nav-item"><a href="#bottom-justified-tab2" class="nav-link" data-toggle="tab">Inactive</a></li>
-                        <li class="nav-item"><a href="#bottom-justified-tab3" class="nav-link" data-toggle="tab">Inactive</a></li>
+                        <li class="nav-item"><a href="#bottom-justified-tab1" class="nav-link active" data-toggle="tab">تنظیمات
+                                عمومی</a></li>
+                        <li class="nav-item"><a href="#bottom-justified-tab2" class="nav-link" data-toggle="tab">Inactive</a>
+                        </li>
+                        <li class="nav-item"><a href="#bottom-justified-tab3" class="nav-link" data-toggle="tab">Inactive</a>
+                        </li>
                     </ul>
 
                     <div class="tab-content">
@@ -21,177 +24,16 @@
                             </div>
 
                             <form action="">
-                                <div class="row pl-20 pr-20 pb-20">
-                                    <div class="col-md-12 mt-10 mb-10">
-                                        <div class="cursor-pointer pick-file" data-toggle="modal"
-                                             data-target="#modal_full"
-                                             style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
-                                            <input class="image-file" type="hidden"
-                                                   name="imgProduct"
-                                                   value="">
-                                            <div class="media stack-media-on-mobile">
-                                                <div class="media-left">
-                                                    <div class="thumb">
-                                                        <a class="display-inline-block"
-                                                           style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
-                                                            <img src=""
-                                                                 class="img-rounded" alt=""
-                                                                 style="width: 100px; height: 100px; object-fit: contain;"
-                                                                 data-base-url="">
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                                <div class="media-body">
-                                                    <h6 class="media-heading">
-                                                        <a class="text-grey-300">
-                                                            انتخاب تصویر:
-                                                        </a>
-                                                        <a class="io-image-name display-block">
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                            </div>
+                                <div class="img-placeholder-group">
+                                    <div class="img-placeholder-custom">
+                                        <div class="img-placeholder-icon-container">
+                                            <i class="img-placeholder-icon icon-image2 text-indigo"></i>
+                                            <span class="img-placeholder-num badge badge-pill bg-warning-400"><i
+                                                        class="icon-plus2"></i></span>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="row pl-20 pr-20 pb-20">
-                                    <div class="col-md-12 mt-10 mb-10">
-                                        <div class="cursor-pointer pick-file" data-toggle="modal"
-                                             data-target="#modal_full"
-                                             style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
-                                            <input class="image-file" type="hidden"
-                                                   name="imgBlog"
-                                                   value="">
-                                            <div class="media stack-media-on-mobile">
-                                                <div class="media-left">
-                                                    <div class="thumb">
-                                                        <a class="display-inline-block"
-                                                           style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
-                                                            <img src=""
-                                                                 class="img-rounded" alt=""
-                                                                 style="width: 100px; height: 100px; object-fit: contain;"
-                                                                 data-base-url="">
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                                <div class="media-body">
-                                                    <h6 class="media-heading">
-                                                        <a class="text-grey-300">
-                                                            انتخاب تصویر:
-                                                        </a>
-                                                        <a class="io-image-name display-block">
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row pl-20 pr-20 pb-20">
-                                    <div class="col-md-12 mt-10 mb-10">
-                                        <div class="cursor-pointer pick-file" data-toggle="modal"
-                                             data-target="#modal_full"
-                                             style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
-                                            <input class="image-file" type="hidden"
-                                                   name="imgFAQ"
-                                                   value="">
-                                            <div class="media stack-media-on-mobile">
-                                                <div class="media-left">
-                                                    <div class="thumb">
-                                                        <a class="display-inline-block"
-                                                           style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
-                                                            <img src=""
-                                                                 class="img-rounded" alt=""
-                                                                 style="width: 100px; height: 100px; object-fit: contain;"
-                                                                 data-base-url="">
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                                <div class="media-body">
-                                                    <h6 class="media-heading">
-                                                        <a class="text-grey-300">
-                                                            انتخاب تصویر:
-                                                        </a>
-                                                        <a class="io-image-name display-block">
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row pl-20 pr-20 pb-20">
-                                    <div class="col-md-12 mt-10 mb-10">
-                                        <div class="cursor-pointer pick-file" data-toggle="modal"
-                                             data-target="#modal_full"
-                                             style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
-                                            <input class="image-file" type="hidden"
-                                                   name="imgContact"
-                                                   value="">
-                                            <div class="media stack-media-on-mobile">
-                                                <div class="media-left">
-                                                    <div class="thumb">
-                                                        <a class="display-inline-block"
-                                                           style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
-                                                            <img src=""
-                                                                 class="img-rounded" alt=""
-                                                                 style="width: 100px; height: 100px; object-fit: contain;"
-                                                                 data-base-url="">
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                                <div class="media-body">
-                                                    <h6 class="media-heading">
-                                                        <a class="text-grey-300">
-                                                            انتخاب تصویر:
-                                                        </a>
-                                                        <a class="io-image-name display-block">
-                                                            <?= basename($values_images['imgContact'] ?? $setting['pages']['contactUs']['topImage'] ?? ''); ?>
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row pl-20 pr-20 pb-20">
-                                    <div class="col-md-12 mt-10 mb-10">
-                                        <div class="cursor-pointer pick-file" data-toggle="modal"
-                                             data-target="#modal_full"
-                                             style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
-                                            <input class="image-file" type="hidden"
-                                                   name="imgComplaint"
-                                                   value="<?= $values_images['imgComplaint'] ?? $setting['pages']['complaint']['topImage'] ?? ''; ?>">
-                                            <div class="media stack-media-on-mobile">
-                                                <div class="media-left">
-                                                    <div class="thumb">
-                                                        <a class="display-inline-block"
-                                                           style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
-                                                            <img src=""
-                                                                 class="img-rounded" alt=""
-                                                                 style="width: 100px; height: 100px; object-fit: contain;"
-                                                                 data-base-url="">
-                                                        </a>
-                                                    </div>
-                                                </div>
-
-                                                <div class="media-body">
-                                                    <h6 class="media-heading">
-                                                        <a class="text-grey-300">
-                                                            انتخاب تصویر:
-                                                        </a>
-                                                        <a class="io-image-name display-block">
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                            </div>
+                                        <div>
+                                            <input class="file-manager-image" type="hidden" name="" value="">
+                                            <!-- <img src="" alt="image" class="img-placeholder-image"> -->
                                         </div>
                                     </div>
                                 </div>
@@ -200,10 +42,7 @@
                                     <hr style="margin-bottom: 0;">
                                     <button type="submit"
                                             class="btn btn-default btn-block pt-20 pb-20 no-border-radius-top">
-                                                <span class="h5">
-                                                <i class="icon-cog position-left"></i>
-                                                    ذخیره تنظیمات
-                                                </span>
+                                        <span class="h5"><i class="icon-cog position-left"></i>ذخیره تنظیمات</span>
                                     </button>
                                 </div>
                             </form>
@@ -211,11 +50,13 @@
                         </div>
 
                         <div class="tab-pane fade" id="bottom-justified-tab2">
-                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid laeggin.
+                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid
+                            laeggin.
                         </div>
 
                         <div class="tab-pane fade" id="bottom-justified-tab3">
-                            DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg whatever.
+                            DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg
+                            whatever.
                         </div>
                     </div>
                 </div>
