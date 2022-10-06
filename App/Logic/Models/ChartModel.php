03db27e9d09b150ab98fbfafd34ebf284fc531df
MMDM <mmdehghan95@gmail.com>
2022-10-06 23:22:37 +0330

    fix: fix show chart query

--- App/Logic/Models/ChartModel.php
+++ App/Logic/Models/ChartModel.php
@@ -26,9 +26,11 @@ class ChartModel extends BaseModel
                 'COUNT(oa.product_code) AS count',
             ])
             ->where('oa.ordered_at BETWEEN :d1 AND :d2')
+            ->where('oa.payment_status=:ps')
             ->bindValues([
                 'd1' => $fromDate,
                 'd2' => $toDate,
+                'ps' => PAYMENT_STATUS_SUCCESS,
             ])
             ->groupBy(['ob.code']);
 
@@ -62,9 +64,11 @@ class ChartModel extends BaseModel
                 'COUNT(DISTINCT(oa.id)) AS count',
             ])
             ->where('oa.ordered_at BETWEEN :d1 AND :d2')
+            ->where('oa.payment_status=:ps')
             ->bindValues([
                 'd1' => $fromDate,
                 'd2' => $toDate,
+                'ps' => PAYMENT_STATUS_SUCCESS,
             ])
             ->orderBy(['3 DESC'])
             ->limit($topN)
